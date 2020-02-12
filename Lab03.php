5279f382a660cbcde85b771b49b4716bda723139
LEON LAW (1603156) <1603156@rgu.ac.uk>
2020-02-12 11:50:44 +0000

    add lab03

---
+++ Lab03.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>My first PHP page</h1>
+
+<?php
+$output = shell_exec('python r.py');
+echo "<p> The python script said </p>";
+echo "<pre>$output</pre>";
+?>
+
+</body>
+</html>
